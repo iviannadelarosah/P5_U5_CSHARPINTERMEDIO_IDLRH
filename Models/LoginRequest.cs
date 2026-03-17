7fa5b07dc9a5a665480f1795a6163cc76311a690
Ivianna de la Rosa Hilario <102991865+iviannadelarosah@users.noreply.github.com>
2026-03-16 23:17:24 -0400

    Delete Models directory

--- Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P5_U5_CSHARPINTERMEDIO_IDLRH.Models
-{
-    public class LoginRequest
-    {
-        public string UsuarioId { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- Models/RefreshRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P5_U5_CSHARPINTERMEDIO_IDLRH.Models
-{
-    public class RefreshRequest
-    {
-        public string Token { get; set; }
-    }
-}
