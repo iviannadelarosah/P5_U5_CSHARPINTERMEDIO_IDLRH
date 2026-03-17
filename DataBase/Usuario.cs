19c97b8c132439a9f73077aa5f7949ea354aafa8
Ivianna de la Rosa <iviannadelarosa@gmail.com>
2026-03-16 23:33:44 -0400

    Agregando proyecto DataBase

---
+++ DataBase/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(30)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+        public DateTime FechaDeNacimiento { get; set; }
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
