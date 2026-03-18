93475da8f3304d2db28ab4c9f78d07c5a3abd984
Ivianna de la Rosa <iviannadelarosa@gmail.com>
2026-03-17 22:40:00 -0400

    Revert "eliminacion de campo no necesario"
    This reverts commit d6998ee8befe0d6c944de9a0059a2f880984ee58.

---
+++ DataBase/ProductoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class ProductoContext : DbContext
+    {
+        public ProductoContext(DbContextOptions<ProductoContext> options) : base(options) { }
+
+        public DbSet<Producto> productos { get; set; }
+        public DbSet<Proveedor> proveedores { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().ToTable("productos");
+            modelBuilder.Entity<Proveedor>().ToTable("proveedores");
+            modelBuilder.Entity<Categoria>().ToTable("categorias");
+        }
+    }
+}
---
+++ DataBase/UsuarioContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options): base(options) { }
+
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().ToTable("usuarios");
+        }
+    }
+}
